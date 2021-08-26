43bd1e865fc037c358f69be318cd70342ee2bfc4
devopskickoff <devopskick@gmail.com>
2021-08-26 21:13:26 +0900

    first commit

---
+++ TestSpring2/src/main/java/com/spring/test/HomeController.java
@@ -0,0 +1,42 @@
+package com.spring.test;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	//@RequestMapping : 주소와 메소드를 연결
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	//문자열 반환 시 특정 설정이 없는 경우 view의 경로로 인식한다.
+	//Model : 데이터를 view에 전달
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
